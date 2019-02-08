66c487990ab4fe1c008567dea2244d30f1b92dbd
bwillstorey <bwillstorey@gmail.com>
2019-02-08 16:50:14 -0600

    js logic update 1

---
+++ assets/javascript/game.js
@@ -0,0 +1,85 @@
+// VARIABLES
+
+var answers = ["brodhi","johnny utah", "skydiving", "reagan mask", "anthony keadis"]
+
+var maxGuesses = 12;
+var numberGuesses
+var hiddenWord = []
+var guessedLetters = []
+var guessingWord = []
+var foundLetter = []
+var winsCurrent = 0;
+
+
+function screenUpdate() {
+    document.getElementById("wins").innerText = winsCurrent;
+    document.getElementById("word").innerText = guessingWord.join("");
+    document.getElementById("guesses").innerText = numberGuesses;
+    document.getElementById("letters").innerText =  guessedLetters.join(" ");
+    }
+
+// Start game
+function startGame() {
+    numberGuesses = maxGuesses;
+    guessingWord = answers[Math.floor(Math.random() * answers.length)].toLowerCase()
+    console.log(guessingWord)
+
+    guessedLetters = []
+    guessingWord = []
+
+    for (var i = 0, j = hiddenWord.length; i < j; i++){
+        if (hiddenWord[i] === " ") {
+            guessingWord.push(" ")
+        } else {
+            guessingWord.push("-")
+        }
+        screenUpdate()
+    }
+    
+    
+}
+
+// MAIN PROCESS
+
+startGame()
+
+// document.onkeyup = function(letter) {
+
+//     // var userInput = event.key.toLowerCase();
+
+
+
+//     if (!foundLetter) {
+//         if (!guessedLetters.includes(letter)) {
+//             guessedLetters.push(letter)
+//             numberGuesses--
+//         }
+//         if (maxGuesses === 0) {
+//             guessingWord = hiddenWord.split()
+//             screenUpdate()
+//         }
+//     }
+// }
+
+document.onkeyup = function(event) {
+
+    findLetter(event.key.toLowerCase())
+
+}
+
+function findLetter(letter) {
+
+    for (var i = 0, j = hiddenWord.length; i < j; i++) {
+        if (letter === hiddenWord[i]) {
+            guessingWord[i] = letter
+
+            if (guessingWord.join("") === guessingWord) {
+                winsCurrent++
+ 
+            }
+        }
+    }
+
+
+}
+
