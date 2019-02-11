a26894f5603bc3b4a3598b0c3112d06bf9ae625c
bwillstorey <bwillstorey@gmail.com>
2019-02-10 18:55:57 -0600

    adding reworked js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,79 @@
 // VARIABLES
 
 var answers = ["brodhi","johnny utah", "skydiving", "reagan mask", "anthony keadis"]
-
 var maxGuesses = 12;
 var numberGuesses
-var guessedLetters
-var guessingWord
-var foundLetter
+var wordToMatch 
+var guessedLetters = []
+var guessingWord = []
 var winsCurrent = 0;
 
 
-function screenUpdate() {
-    document.getElementById("wins").innerText = winsCurrent;
-    document.getElementById("word").innerText = guessingWord.join("");
-    document.getElementById("guesses").innerText = numberGuesses;
-    document.getElementById("letters").innerText =  guessedLetters.join(" ");
-    }
+resetGame()
 
-// Start game
-function startGame() {
-    numberGuesses = maxGuesses;
-    guessingWord = answers[Math.floor(Math.random() * answers.length)].toLowerCase()
-    console.log(guessingWord)
+// event listener for key up from user
+document.onkeypress = function(event) {
 
-    guessedLetters = []
-    guessingWord = []
-
-    for (var i = 0, j = guessingWord.length; i < j; i++){
-        if (guessingWord[i] === " ") {
-            guessingWord.push(" ")
-        } else {
-            guessingWord.push("-")
-        }
-        screenUpdate()
-    }
-}
-
-// MAIN PROCESS
-
-startGame()
-
-document.onkeyup = function(event) {
+    // only allows a-z to be marked as a guess
     var inp = String.fromCharCode(event.keyCode);
     if (/[a-zA-Z]/.test(inp)) {
         findLetter(event.key.toLowerCase())
     }
-
-
 }
 
-function findLetter(letter) {
-
-    for (var i = 0, j = answers.length; i < j; i++) {
-        if (letter === guessingWord[i]) {
-            guessingWord[i] = letter
-            foundLetter = true
-            if (guessingWord.join("") === answers) {
-                winsCurrent++
-                screenUpdate()
+    function findLetter(letter) {
+        var foundLetter = false
+        // searches the hidden word for the letter pressed by user
+        for (var i = 0, j = wordToMatch.length; i < j; i++) {
+            if (letter === wordToMatch[i]) {
+                guessingWord[i] = letter
+                foundLetter = true
+                if (guessingWord.join("") === wordToMatch) {
+                    winsCurrent++
+                    screenUpdate()
+                }
+            }
+        if (!foundLetter) {
+            if (!guessedLetters.includes(letter)) {
+                guessedLetters.push(letter)
+                numberGuesses--
+            }
+            if (numberGuesses === 0) {
+                guessingWord = wordToMatch.split()
             }
         }
     }
-
-    if (!foundLetter) {
-        if (!guessedLetters.includes(letter)) {
-            guessedLetters.push(letter)
-            numberGuesses--
-        }
-        if (maxGuesses === 0) {
-            guessingWord = answers.split()
-        }
+        screenUpdate()
     }
 
+// Starts the game
+function resetGame() {
+    numberGuesses = maxGuesses;
+
+    // reassigns hidden word with new word value
+    wordToMatch = answers[Math.floor(Math.random() * answers.length)].toLowerCase()
+    console.log(wordToMatch)
+
+    // resets guessing variables
+    guessedLetters = []
+    guessingWord = []
+
+    // resets word being guessed 
+    for (var i = 0, j = wordToMatch.length; i < j; i++){
+        if (wordToMatch[i] === " ") {
+            guessingWord.push(" ")
+        } else {
+            guessingWord.push("-")
+        }
+    }
+    
     screenUpdate()
 }
 
+// updates the screen when user presses keys
+function screenUpdate() {
+    document.getElementById("wins").innerText = winsCurrent
+    document.getElementById("word").innerText = guessingWord.join("")
+    document.getElementById("guesses").innerText = numberGuesses
+    document.getElementById("letters").innerText =  guessedLetters.join(" ")
+    }
---
+++ assets/javascript/game1.js
@@ -0,0 +1,102 @@
+// Define variable
+var possibleWords = ["brodhi","johnny utah", "skydiving", "reagan mask", "anthony keadis"]
+var maxGuess = 10
+var pauseGame = false
+var guessedLetters = []
+var guessingWord = []
+var wordToMatch
+var numGuess
+var wins = 0
+
+resetGame()
+
+document.onkeypress = function(event) {
+ 
+    var inp = String.fromCharCode(event.keyCode);
+
+    if (/[a-zA-Z]/.test(inp)) {
+        checkForLetter(event.key.toUpperCase())
+    }
+}
+
+function checkForLetter(letter) {
+    var foundLetter = false
+    
+    for (var i = 0, j = wordToMatch.length; i < j; i++) {
+        if (letter === wordToMatch[i]) {
+            guessingWord[i] = letter
+            foundLetter = true
+            if (guessingWord.join("") === wordToMatch) {
+                wins++
+                displayPicture()
+                pauseGame = true
+                updateDisplay()
+                resetGame()
+            }
+        }
+    }
+
+    if (!foundLetter) {
+        if (!guessedLetters.includes(letter)) {
+            guessedLetters.push(letter)
+            numGuess--
+        }
+        if (numGuess === 0) {
+            guessingWord = wordToMatch.split()
+            pauseGame = true
+            resetGame()
+        }
+    }
+
+    updateDisplay()
+
+}
+
+
+
+function resetGame() {
+    numGuess = maxGuess
+    pauseGame = false
+
+    wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].toUpperCase()
+    console.log(wordToMatch)
+
+    guessedLetters = []
+    guessingWord = []
+
+    for (var i = 0, j = wordToMatch.length; i < j; i++) {
+        if (wordToMatch[i] === " ") {
+            guessingWord.push(" ")
+        }
+        else {
+            guessingWord.push("_")
+        }
+    }
+
+    updateDisplay()
+}
+
+function updateDisplay () {
+    document.getElementById("wins").innerText = wins
+    document.getElementById("word").innerText = guessingWord.join("")
+    document.getElementById("guesses").innerText = numGuess
+    document.getElementById("letters").innerText = guessedLetters.join(" ")
+}
+
+function displayPicture () {
+    if (wordToMatch === possibleWords[0].toUpperCase()) {
+        document.getElementById("swayze").style.display = "block";
+    }
+    else if (wordToMatch === possibleWords[1].toUpperCase()) {
+        document.getElementById("keanu").style.display = "block";
+    }
+    else if (wordToMatch === possibleWords[2].toUpperCase()) {
+        document.getElementById("skydive").style.display = "block";
+    }
+    else if (wordToMatch === possibleWords[3].toUpperCase()) {
+        document.getElementById("reagan").style.display = "block";
+    }
+    else if (wordToMatch === possibleWords[4].toUpperCase()) {
+        document.getElementById("kiedis").style.display = "block";
+    }
+}
